d5c34adff061490cf1e58a913c2d7f60e04af379
Sayan Yadav <yadavsayan9@gmail.com>
2026-07-14 13:11:37 +0530

    bounding box

---
+++ .ipynb_checkpoints/utilities_mask_r-cnn-checkpoint.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import torch 
+import random
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+def get_torch_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    else:
+        return torch.device("cpu")
+
+# def get_img_files(dataset_path):
+#     image_data = []
+
+#     for filename in os.listdir(folder_path):
+#         if filename.endswith(".jpg") or filename.endswith(".png"):
+#             img_path = os.path.join(folder_path, filename)
+#             img = Image.open(img.path).convert("RGB")
+#             img_array = np.array(img)
+#             image_data.append(img_array)
+#         image_data = np.array(image_data)
+#     return image_data
+def get_img_files(folder_path):
+    folder_path = Path(folder_path)
+    img_files = [f for f in folder_path.glob('*') 
+                 if f.suffix.lower() in ['.jpg', '.png']]
+    return sorted(img_files)
